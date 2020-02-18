7c29f3f153e59759d3c53b354c405a4ef75d5a8a
Abylaikhan Zulbukharov <l1pt0n1905@gmail.com>
2020-02-19 02:39:56 +0600

    feat: add what-seados solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,8 +1,15 @@
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  //   throw 'Not implemented';
-  // remove line with error and write your code here
-  return 0;
+module.exports = function dateSample(sampleActivity) {
+  throw "Not implemented";
+  if (typeof sampleActivity !== "string") return false;
+  let s = parseFloat(sampleActivity);
+  let m = MODERN_ACTIVITY;
+  let y = 0;
+  while (m > s) {
+    m = m / 2;
+    y += HALF_LIFE_PERIOD;
+  }
+  return y;
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,16 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+const isNotValid = d => !(d instanceof Date) || isNaN(d.getTime());
+
+module.exports = function getSeason(date) {
+  if (!date) return "Unable to determine the time of year!";
+  if (isNotValid(date)) throw new Error("Error");
+  // spring
+  // summer
+  // autumn
+  // winter
+  let s = "spring";
+  let m = date.getMonth() + 1; // starts from 0
+  if (m === 12 || m <= 2) s = "winter";
+  else if (m >= 9 && m <= 11) s = "autumn";
+  else if (m >= 6 && m <= 8) s = "summer";
+  return s;
 };

4a10ee5608321acdebe56efa10ae1dbeecf946ea
Abylaikhan Zulbukharov <l1pt0n1905@gmail.com>
2020-02-25 01:36:41 +0600

    feat: add recursive-depth solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,19 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+	// d => array of each branch depth (DFS)
+	// i => current branch depth
+	// a => current array
+	calculateDepth(a, i = 1, d = [1]) {
+		a.forEach(v => {
+			if (Array.isArray(v)) {
+				d.push(i + 1);
+				this.calculateDepth(v, i + 1, d);
+			}
+		})
+		return getMaxOfArray(d);
+	}
 };
+
+// get max of array
+function getMaxOfArray(numArray) {
+	return Math.max.apply(null, numArray);
+}

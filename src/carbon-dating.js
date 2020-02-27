0a7d1e7a3a50e4430d71e908e3c196459a776028
Abylaikhan Zulbukharov <l1pt0n1905@gmail.com>
2020-02-27 17:07:35 +0600

    feat: add vegenere solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,14 +2,14 @@ const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  throw "Not implemented";
-  if (typeof sampleActivity !== "string") return false;
-  let s = parseFloat(sampleActivity);
-  let m = MODERN_ACTIVITY;
-  let y = 0;
-  while (m > s) {
-    m = m / 2;
-    y += HALF_LIFE_PERIOD;
-  }
-  return y;
+	throw "Not implemented";
+	if (typeof sampleActivity !== "string") return false;
+	let s = parseFloat(sampleActivity);
+	let m = MODERN_ACTIVITY;
+	let y = 0;
+	while (m > s) {
+		m = m / 2;
+		y += HALF_LIFE_PERIOD;
+	}
+	return y;
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,45 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+	constructor(r) {
+		this.reverse = r;
+	}
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+	encrypt(s, k) {
+		let key = s.length > k.length ? k.repeat(Math.ceil(s.length / k.length)).slice(0, s.length) : k;
+		let i = 0;
+		let a = [...s].map((c) => {
+			// if not a-z
+			if (!(c.match(/[a-z]/i)))
+				return c;
+			let k = key.charCodeAt(i);
+			let s = c.charCodeAt(0);
+			let d = (k - (k >= 97 && k <= 122 ? 97 : 65)) + (s - (s >= 97 && k <= 122 ? 97 : 65));
+			i++;
+			return String.fromCharCode((s >= 97 && k <= 122 ? 97 : 65) + (d % 26));
+		});
+		return a.join('').toUpperCase();
+	}
+
+	decrypt(s, k) {
+		s = this.reverse === false ? [...s].reverse().join('') : s;
+		let key = s.length > k.length ? k.repeat(Math.ceil(s.length / k.length)).slice(0, s.length) : k;
+		let i = 0;
+		let a = [...s].map((c) => {
+			// if not a-z
+			if (!(c.match(/[a-z]/i)))
+				return c;
+			let k = key.charCodeAt(i);
+			let s = c.charCodeAt(0);
+			s = s - (s >= 97 && s <= 122 ? 97 : 65);
+			k = k - (k >= 97 && k <= 122 ? 97 : 65);
+			let d = s - k;
+			i++;
+			d = d < 0 ? 26 + d : d;
+			return String.fromCharCode(65 + (d % 26));
+		});
+		if (this.reverse === false)
+			return a.reverse().join('').toUpperCase();
+		return a.join('').toUpperCase();
+	}
 }
 
 module.exports = VigenereCipheringMachine;
